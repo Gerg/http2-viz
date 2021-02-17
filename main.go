7ad8cacb496d2f509ec8f8ebf5bd56fa390adc3a
Greg Cobb <gcobb@pivotal.io>
2021-02-16 17:11:20 -0800

    Refactor to extract common logic
    Authored-by: Greg Cobb <gcobb@pivotal.io>

--- main.go
+++ main.go
@@ -20,16 +20,29 @@ const serverPort = ":8000"
 const proxyPort = ":8001"
 const clientPort = ":8002"
 
-type Client struct{}
-type Proxy struct{}
-type Server struct{}
+type Http2Server struct{ Port string }
+type Client struct {
+	Http2Server
+	ProxyPort string
+}
+type Proxy struct {
+	Http2Server
+	ServerPort string
+}
+type Server struct{ Http2Server }
 
 func main() {
 	waitGroup := sync.WaitGroup{}
 
-	client := Client{}
-	proxy := Proxy{}
-	server := Server{}
+	client := Client{
+		Http2Server: Http2Server{Port: clientPort},
+		ProxyPort:   proxyPort,
+	}
+	proxy := Proxy{
+		Http2Server: Http2Server{Port: proxyPort},
+		ServerPort:  serverPort,
+	}
+	server := Server{Http2Server: Http2Server{Port: serverPort}}
 
 	waitGroup.Add(3)
 	go client.start(&waitGroup)
@@ -42,22 +55,18 @@ func main() {
 func (c Client) start(waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 
-	srv := &http.Server{Addr: clientPort, Handler: http.HandlerFunc(c.handle)}
-
-	log.Printf("Starting client on https://0.0.0.0%s", clientPort)
-	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+	serveHttp(c.Http2Server.Port, "client", c.handle)
 }
 
 func (c Client) handle(w http.ResponseWriter, r *http.Request) {
-	url := fmt.Sprintf("https://localhost%s", proxyPort)
+	url := fmt.Sprintf("https://localhost%s", c.ProxyPort)
 	response := c.makeRequest(url)
 	fmt.Fprint(w, string(response))
 }
 
 func (c Client) makeRequest(url string) string {
 	client := &http.Client{}
-
-	c.configureTLS(client)
+	client.Transport = buildTlsTransport()
 
 	resp, err := client.Get(url)
 	if err != nil {
@@ -79,36 +88,16 @@ func (c Client) makeRequest(url string) string {
 	return string(stringifiedResponse)
 }
 
-func (c Client) configureTLS(client *http.Client) {
-	caCert, err := ioutil.ReadFile("server.crt")
-	if err != nil {
-		log.Fatalf("Reading server certificate: %s", err)
-	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-
-	tlsConfig := &tls.Config{
-		RootCAs: caCertPool,
-	}
-
-	client.Transport = &http2.Transport{
-		TLSClientConfig: tlsConfig,
-	}
-}
-
 func (p Proxy) start(waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 
-	srv := &http.Server{Addr: proxyPort, Handler: http.HandlerFunc(p.handle)}
-
-	log.Printf("Starting proxy on https://0.0.0.0%s", proxyPort)
-	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+	serveHttp(p.Http2Server.Port, "proxy", p.handle)
 }
 
 func (p Proxy) handle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Proxy request protocol: %s\n", r.Proto)
 
-	origin, _ := url.Parse(fmt.Sprintf("http://localhost%s", serverPort))
+	origin, _ := url.Parse(fmt.Sprintf("http://localhost%s", p.ServerPort))
 
 	director := func(req *http.Request) {
 		req.Header.Add("X-Forwarded-Host", req.Host)
@@ -118,12 +107,29 @@ func (p Proxy) handle(w http.ResponseWriter, r *http.Request) {
 	}
 
 	proxy := &httputil.ReverseProxy{Director: director}
-	p.configureTLS(proxy)
+	proxy.Transport = buildTlsTransport()
 
 	proxy.ServeHTTP(w, r)
 }
 
-func (p Proxy) configureTLS(proxy *httputil.ReverseProxy) {
+func (s Server) start(waitGroup *sync.WaitGroup) {
+	defer waitGroup.Done()
+
+	serveHttp(s.Http2Server.Port, "server", s.handle)
+}
+
+func (s Server) handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server request protocol: %s\n", r.Proto)
+}
+
+func serveHttp(port string, name string, handler http.HandlerFunc) {
+	srv := &http.Server{Addr: port, Handler: http.HandlerFunc(handler)}
+
+	log.Printf("Starting %s on https://0.0.0.0%s", name, port)
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+}
+
+func buildTlsTransport() *http2.Transport {
 	caCert, err := ioutil.ReadFile("server.crt")
 	if err != nil {
 		log.Fatalf("Reading server certificate: %s", err)
@@ -135,20 +141,7 @@ func (p Proxy) configureTLS(proxy *httputil.ReverseProxy) {
 		RootCAs: caCertPool,
 	}
 
-	proxy.Transport = &http2.Transport{
+	return &http2.Transport{
 		TLSClientConfig: tlsConfig,
 	}
-}
-
-func (s Server) start(waitGroup *sync.WaitGroup) {
-	defer waitGroup.Done()
-
-	srv := &http.Server{Addr: serverPort, Handler: http.HandlerFunc(s.handle)}
-
-	log.Printf("Starting server on https://0.0.0.0%s", serverPort)
-	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
-}
-
-func (s Server) handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Server request protocol: %s\n", r.Proto)
 }
